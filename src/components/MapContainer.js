2d2178f5ccfabd173bc72d57c117f505ec11637d
na_0_i <nayoi1oo5@gmail.com>
2022-02-14 17:54:07 +0900

    get weather information

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,27 +1,29 @@
 import React, { useState } from "react";
 import { useEffect } from 'react';
 import './MapContainer.css';
-
+import axios from "axios";
 
 function MapContainer(){
-  let [currentLocation, updateLocation] = useState('');
+  let [currentLocation] = useState(['위도', '경도']);
+  let [currentDirectory, updatecurrentDirectory] = useState('');
+  let [currentWeather] = useState('맑음');
+
   function getLocation(){
     let nowLatitude = '';
     let nowLongitude = '';
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
-        nowLatitude = position.coords.latitude;
-        nowLongitude = position.coords.longitude;
+        currentLocation[0] = position.coords.latitude;
+        currentLocation[1] = position.coords.longitude;
 
         /*global kakao*/
         const geocoder = new kakao.maps.services.Geocoder();
         const callback = function(result, status) {
           if (status === kakao.maps.services.Status.OK) {
-            let newArray = [...currentLocation];
+            let newArray = [...currentDirectory];
             newArray[0] = result[0].address_name;
-            updateLocation(newArray);
+            updatecurrentDirectory(newArray);
             // currentLocation = result[0].address_name;
-            // console.log(currentLocation);
           }
         };
         geocoder.coord2RegionCode(nowLongitude, nowLatitude, callback);     
@@ -32,13 +34,28 @@ function MapContainer(){
 
   };
 
+  function getWeather(){
+    axios({
+      url: 'https://api.openweathermap.org/data/2.5/weather?lat=' + currentLocation[0] + '&lon=' + currentLocation[1] + '&appid=f53c6e973671e4859c85ac4761fac792',
+      method: 'get',
+    })
+    .then((res) => {
+      console.log(res.data.weather);
+    })
+    .catch((err) => console.log(err))
+  }
+
   useEffect(() => {
     getLocation();
+    getWeather();
   });
 
   return (
     <div className="map">
-      {currentLocation}
+      {currentDirectory}
+      <br></br>
+      {currentLocation[0]}<br></br>
+      {currentLocation[1]}
     </div>
   )
 };
