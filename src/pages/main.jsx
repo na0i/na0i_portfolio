a4ccce0dfaceb59a8a6ccd979c115d2ad39bd82b
na_0_i <nayoi1oo5@gmail.com>
2022-06-27 18:39:04 +0900

    add: main.jsx

---
+++ src/pages/main.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+import ProgressBar from "src/components/progressBar";
+import Profile from "src/components/profile";
+
+const Main = () => {
+  return (
+    <Container>
+      <ProgressBar />
+      <Profile />
+    </Container>
+  );
+};
+
+export default Main;
+
+const Container = styled.div`
+  font-family: "LeferiPointWhite";
+  transform: skew(-0.02deg);
+  padding: 20px;
+`;
