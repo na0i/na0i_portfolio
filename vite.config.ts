5c20f3ff27a6b13112278fa86e979a82325beb0e
na_0_i <nayoi1oo5@gmail.com>
2025-08-11 00:56:33 +0900

    feat: viteconfig에 배포를 위한 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/na0i_portfolio/",
   plugins: [react()],
   resolve: {
     alias: {
